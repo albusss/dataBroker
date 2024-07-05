b5e76011d29b8e8b0008c8d54db61e93ac1483e1
Anatoly Chulkov <anatoly.chulkov@rightfusion.com>
2024-07-05 18:04:36 +0300

    added clustrmaps parser and parsing logic

--- src/Contract/Dictionary/ParserType.php
+++ src/Contract/Dictionary/ParserType.php
@@ -6,12 +6,10 @@ namespace App\Contract\Dictionary;
 
 enum ParserType
 {
-//    case Parser411;// cloudflare protection
     case ParserAbcheck;
     case ParserAddresses;
     case ParserAdvancedPeopleSearch;
-//    case ParserAmericaphonebook;
     case ParserAnywho;
-//    case ParserBackgroundalert;
     case ParserBackgroundcheck;
+    case ParserClustrmaps;
 }
