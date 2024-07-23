bb75bdf65f719fda915e13db23ce7ccda5acc514
Anatoly Chulkov <anatoly.chulkov@rightfusion.com>
2024-07-23 17:19:39 +0300

    updated instantcheckmate.js parser

--- src/Parser/instantcheckmate.js
+++ src/Parser/instantcheckmate.js
@@ -3,15 +3,12 @@ const path = require('path');
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
-const funcs = require('./functions');
 const logger = require('./other/logger');
 
 let rawdata = fs.readFileSync(path.resolve(__dirname, './config.json'));
 let config = JSON.parse(rawdata);
 let browser, page;
-// let proxyNumber = funcs.randomInt(0, 1);
-// 0 - cheaper proxy, 1 - expensive proxy
-let proxyNumber = 0;
+let proxyNumber = Math.floor(Math.random() * config.proxy.length);
 let firstname = process.argv[2];
 let lastname = process.argv[3];
 let city = process.argv[4];
@@ -74,17 +71,17 @@ let state = process.argv[5];
             password: config.proxy[proxyNumber].pass
         });
 
-        await page.goto(`https://www.instantcheckmate.com/results?firstName=${firstname}&middleInitial=&lastName=${lastname}&city=${city}&state=${state}`)
+        await page.goto(`https://www.instantcheckmate.com/results/?firstName=${firstname}&lastName=${lastname}&city=${city}&state=${state}`);
 
         await page.waitForSelector('#people');
 
         const results = await page.evaluate(() => {
             let titleNodeList = Array.from(document.querySelectorAll('div.person'));
             let res = [];
-            titleNodeList.map((td, index) => {
+            titleNodeList.map(td => {
                 const nameNode = td.querySelector('li.category.name > h4');
                 const ageNode = td.querySelector('span.display-age');
-                const locationNode = td.querySelector('li.category.location > ul');
+                const locationNode = td.querySelector('.address');
 
                 const name = nameNode ? nameNode.textContent.trim() : '';
                 const age = ageNode ? ageNode.textContent.trim() : '';
