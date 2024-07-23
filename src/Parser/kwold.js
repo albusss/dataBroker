304e2fe1014c70cc286dc2c933d555ef4165f6e3
Anatoly Chulkov <anatoly.chulkov@rightfusion.com>
2024-07-23 16:50:47 +0300

    updated kwold.js parser

--- src/Parser/kwold.js
+++ src/Parser/kwold.js
@@ -3,28 +3,23 @@ const path = require('path');
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
-const funcs = require('./functions');
 const logger = require('./other/logger');
 
 let rawdata = fs.readFileSync(path.resolve(__dirname, './config.json'));
 let config = JSON.parse(rawdata);
 let browser, page;
-// let proxyNumber = funcs.randomInt(0, 1);
-// 0 - cheaper proxy, 1 - expensive proxy
-let proxyNumber = 0;
+let proxyNumber = Math.floor(Math.random() * config.proxy.length);
 let firstname = process.argv[2];
 let lastname = process.argv[3];
 let city = process.argv[4];
 let state = process.argv[5];
-
-const webpageURL = `https://kwold.com/profile/search?fname=${firstname}&lname=${lastname}&state=${state}&city=${city}&fage=`;
 
 (async () => {
     try {
 
         browser = await puppeteer.launch({
             slowMo: 100,
-            headless: false,
+            headless: true,
             devtools: true,
             args: [
                 '--proxy-server=' + config.proxy[proxyNumber].host,
@@ -73,19 +68,20 @@ const webpageURL = `https://kwold.com/profile/search?fname=${firstname}&lname=${
             password: config.proxy[proxyNumber].pass
         });
 
-        await page.goto(webpageURL)
+        await page.goto(`https://kwold.com/profile/search?fname=${firstname}&lname=${lastname}&${state}=TX&city=${city}`);
+
         await page.waitForSelector('.card');
 
         const results = await page.evaluate(() => {
             let profileList = Array.from(document.querySelectorAll('.card')).slice(0, 10);
             let res = [];
             profileList.forEach((profile) => {
-                let name = profile.querySelector("span[itemprop=givenName]")?.textContent + ' ' + profile.querySelector("span[itemprop=familyName]")?.textContent;
-                let age = profile.querySelector('.card-title')?.textContent?.split('~')?.[1]?.split(' ')?.[0];
-                let address = profile.querySelector("span[itemprop=address]")?.textContent?.trim();
+                let name = profile.querySelector("span[itemprop=givenName]").textContent + ' ' + profile.querySelector("span[itemprop=familyName]").textContent;
+                let age = profile.querySelector('.card-title').textContent.split('~')[1].split(' ')[0];
+                let address = profile.querySelector("span[itemprop=address]").textContent.trim();
                 let link = Array.from(profile.querySelectorAll('a'))
                     .find((elem) => elem.textContent.includes('View Profile'))
-                    ?.href
+                    .href
                 res.push({name, address, link, age});
             })
             return res;
